d428545c18e85edc6364ec66253848844b9926da
xyz123-jpg <dbuljan.buljan2@gmail.com>
2024-06-19 13:32:49 +0200

    Start

---
+++ KingICT_akademija/src/main/java/com/example/kingict_akademija/KingIctAkademijaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kingict_akademija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingIctAkademijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingIctAkademijaApplication.class, args);
+	}
+
+}
---
+++ KingICT_akademija/src/test/java/com/example/kingict_akademija/KingIctAkademijaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kingict_akademija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingIctAkademijaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
